c831be8ba3f4d046686b1d81ef3f01dfe6f0e86d
Shuri <alexandresantoslealfilho@gmail.com>
2024-11-22 20:52:16 -0300

    Mudança da database interno para o Atlas

--- server.js
+++ server.js
@@ -1,58 +1,33 @@
 import express from "express";
- 
-
+import conectarAoBanco from "./src/config/dbconfig.js";
+const VariavelConexao = process.env.STRING_CONEXAO;
+const conexao = await conectarAoBanco(VariavelConexao)
 
 const app = express();
 app.use(express.json());
-
-const posts = [
-    {
-      id: 1,
-      descricao: "Uma foto de um gato",
-      imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-      id: 2,
-      descricao: "Uma paisagem montanhosa",
-      imagem: "https://picsum.photos/seed/picsum/200/300"
-    },
-    {
-      id: 3,
-      descricao: "Um prato de comida deliciosa",
-      imagem: "https://source.unsplash.com/random/200x300/?food"
-    },
-    {
-      id: 4,
-      descricao: "Uma cidade à noite",
-      imagem: "https://unsplash.com/photos/night-city"
-    },
-    {
-      id: 5,
-      descricao: "Um pôr do sol na praia",
-      imagem: "https://unsplash.com/s/photos/sunset"
-    },
-    {
-      id: 6,
-      descricao: "Um animal selvagem na natureza",
-      imagem: "https://unsplash.com/s/photos/wildlife"
-    }
-];
 
 app.listen(3000, () => {
     console.log("Servidor escutando!");
 });
 
-app.get("/posts", (req,res) =>{
+async function getPosts(){
+    const db = conexao.db("Imersão-InstaByte")
+    const colecao = db.collection("posts")
+    return colecao.find().toArray()
+}
+
+app.get("/posts", async(req,res) =>{
+  const posts = await getPosts()
     res.status(200).json(posts);
 });
 
-function localizarPostPorId(id){
-  return posts.findIndex((post) =>{
-      return post.id === Number(id)
-  })
-};
+//function localizarPostPorId(id){
+//  return posts.findIndex((post) =>{
+//      return post.id === Number(id)
+//  })
+//};
 
-app.get("/posts/:id", (req, res) =>{
-    const index = localizarPostPorId(req.params.id);
-    res.status(200).json(posts[index]);
-});
+//app.get("/posts/:id", (req, res) =>{
+//    const index = localizarPostPorId(req.params.id);
+//    res.status(200).json(posts[index]);
+//});
