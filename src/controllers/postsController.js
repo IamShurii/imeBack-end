916e3cefdbc8500bcacdc7550d14d14312d505f3
Shuri <alexandresantoslealfilho@gmail.com>
2024-11-23 23:47:08 -0300

    começando postsController

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -4,3 +4,8 @@ export async function listarPosts (req,res){
     const posts = await getPosts();
     res.status(200).json(posts);
 };
+
+export async function criarNovoPost(req, res) {
+    const novoPost = req.body();
+    res.status
+}
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -4,4 +4,5 @@ import { listarPosts } from "../controllers/postsController.js";
 export default function routes(app){
     app.use(express.json());
     app.get("/posts",listarPosts)
+    app.post("/posts",)
 }
