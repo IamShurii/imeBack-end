a939f8c33840203455f817cc57d6e3cb052ef716
Shuri <alexandresantoslealfilho@gmail.com>
2024-11-26 02:05:59 -0300

    set 3000 as a variable port

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import routes from "./src/routes/postsRoutes.js"
-
+const port = process.env.PORT || 3000;
 const app = express()
 
 app.use(express.static("uploads"))
 
 routes(app)
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Servidor escutando!");
 });
